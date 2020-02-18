d2b9be9f55aa8ef43cc00fc38abe03e83178cb3a
JairSim_777 <capostst@gmail.com>
2020-02-18 12:06:07 -0500

    Tabla buscador

---
+++ 4sproyecto/newsproyecto.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#example').DataTable({
+        "langueje": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "opaginate": {
+                "sFirst": "Primero",
+                "sLast": "Ultimo",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Procesando...",
+        }
+    });
+} );
