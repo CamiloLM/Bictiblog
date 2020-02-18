a9e5daa5f180965122fc1349ff2d3e4dc69b8a81
yuliano <yulianosalvatore2001@gmail.com>
2020-02-18 12:03:13 -0500

    actualisacion del login

--- login/login.php
+++ login/login.php
@@ -1,28 +1,31 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass ="";
-$dbname ="bictiblog";
+$server = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'bictiblog';
 
+$conn = mysqli_connect($server, $user, $pass, $db);
 
+if (isset($_POST) && !empty($_POST)) {
+    $nombre = $_POST ["tnombre"];
+    $contrasena= $_POST["usupas"];
 
-$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-if(!$conn)
-{
-    die("no hay coneccion :".mysqli_connect_error());
-
-}
-$nombre = $_POST ["tnombre"];
-$contrasena= $_POST["usupas"];
-$query = mysqli_query($conn,"SELECT * FROM usuarios WHERE nombre= '".$nombre."' and contrasena= '".$contrasena."'");
-$nr = mysqli_num_rows($query);
-
-if($nr == 1)
-{
-header ("Location:index.php");
-}
-else if($nr == 0)
-{
-    header ("Location:login.html");
+    $sql = "SELECT * FROM usuarios WHERE nombre= '.$nombre.' and contrasena= '.$contrasena.'";
+    $result = mysqli_query($conn, $sql);
+    
+    if(mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($row['rol'] === 'Admin') {
+                $_SESSION['LoginUser'] = $row["Username"];
+                header('Location:../admin.php');
+            }
+            if ($row['rol'] === 'User') {
+                $_SESSION['LoginUser'] = $row["Username"];
+                header('Location:../user.php');
+            }
+        }
+    } else {
+        header('Location: login.html');
+    }
 }
 ?>
